c049de22598a6686b368f02de4b50f8753cc1925
amora <am2020-0072@uce.edu.do>
2024-07-18 19:06:51 -0400

    Validaciones en formulario de doctores

--- Meditrack/Models/Paciente.cs
+++ Meditrack/Models/Paciente.cs
@@ -1,20 +1,36 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using Meditrack.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace Meditrack.Models
 {
     public partial class Paciente
     {
         public int IdPaciente { get; set; }
+
+        [Required(ErrorMessage = "El nombre del paciente es requerido")]
         public string NombrePaciente { get; set; } = null!;
-        public DateTime? FechaNacimiento { get; set; }
-        public string? SexoPaciente { get; set; }
-        public int? EdadPaciente { get; set; }
+
+        [ValidacionFechaNacimiento(ErrorMessage = "La fecha de nacimiento no puede ser superior a la actual")]
+        public DateTime? FechaNacimiento { get; set; } = null!;
+
+        [Required(ErrorMessage = "El sexo del paciente es requerido")]
+        public string? SexoPaciente { get; set; } = null!;
+
+        [Required(ErrorMessage = "La edad del paciente es requerida")]
+        public int? EdadPaciente { get; set; } = null!;
+
         public string? IdentificacionPaciente { get; set; }
-        public string? NacionalidadPaciente { get; set; }
+
+        [Required(ErrorMessage = "La nacionalidad del paciente es requerida")]
+        public string? NacionalidadPaciente { get; set; } = null!;
         public string? TelefonoPaciente { get; set; }
-        public string? TipoSanguineo { get; set; }
-        public string? SeguroMedico { get; set; }
+
+        [Required(ErrorMessage = "El tipo sanguineo del paciente es requerido")]
+        public string? TipoSanguineo { get; set; } = null!;
+        public string? SeguroMedico { get; set; } 
         public string? HistorialMedico { get; set; }
     }
 }
---
+++ Meditrack/Validation/ValidacionFechaNacimiento.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace Meditrack.Validation
+{
+    public class ValidacionFechaNacimiento : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is DateTime dateTime)
+            {
+                return dateTime <= DateTime.Now;
+            }
+
+            return true;
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return $"La fecha de {name} no puede ser posterior a la fecha actual.";
+        }
+
+    }
+}

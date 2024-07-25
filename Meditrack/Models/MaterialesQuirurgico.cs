1af4dd5e99a2a586f0f671ee90c0e0a845f96e9f
Karlos-Peguero <kp2020-0430@uce.edu.do>
2024-07-25 16:48:25 -0400

    materiales Q

---
+++ Meditrack/Models/MaterialesQuirurgico.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Meditrack.Models
+{
+    public partial class MaterialesQuirurgico
+    {
+        public int IdMaterial { get; set; }
+        public string NombreMaterial { get; set; } = null!;
+        public string? Descripcion { get; set; }
+        public int? Movimiento { get; set; }
+        public int? CantidadDisponibleMaterialQuirurgico { get; set; }
+        public int? IdTipoMaterialQuirurgico { get; set; }
+
+        public virtual TipodeMaterialQuirurgico? IdTipoMaterialQuirurgicoNavigation { get; set; }
+    }
+}

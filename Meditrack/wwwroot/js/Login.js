f9e8e4333da05af1600e209a96cb30c9ed5654e3
Karlos-Peguero <kp2020-0430@uce.edu.do>
2024-07-31 16:34:30 -0400

    .

---
+++ Meditrack/wwwroot/js/Login.js
@@ -0,0 +1,19 @@
+﻿namespace Meditrack.wwwroot.js
+{
+    const container = document.querySelector(".container");
+
+    const btnSignIn = document.getElementById("btn-sign-in");
+    const btnSignUp = document.getElementById("btn-sign-up");
+
+    btnSignIn.addEventListener("click", () => {
+
+        container.classList.remove("toggle");
+
+    });
+
+    btnSignUp.addEventListener("click", () => {
+
+        container.classList.add("toggle");
+
+    });
+}
